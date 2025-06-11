2b8b4dac8b043e7b9d311ea483aaf7a7b73542e1
Israel Adebayo <isszzyraelite@gmail.com>
2025-06-11 11:56:32 +0100

    overview-page v4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import type { ReactNode } from "react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -19,11 +8,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased p-10`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased p-10`}>{children}</body>
     </html>
   );
 }
